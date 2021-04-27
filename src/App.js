34d4a07a4717367446c37e54a9504b767d6f2454
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-04-26 19:50:36 -0500

    added borders to table

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ function App() {
       <h4>My Way</h4>
 
       <div className="container board">
+        <table class="table table-bordered table-hover">
+            <tr>
+                <td>X</td>
+                <td>X</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>X</td>
+                <td>X</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>X</td>
+                <td>X</td>
+                <td>X</td>
+            </tr>
 
+        </table>
       </div>
     </div>
   );
